506c3fcf8c184aaadd544fd679b282c86d4b81c0
Mallory Zlomanchuk <mzlomanchuk@hotmail.com>
2022-07-15 03:07:25 -0600

    adjusting styles

--- form-builder-dashboard/src/components/form-builder/Form.js
+++ form-builder-dashboard/src/components/form-builder/Form.js
@@ -1,8 +1,6 @@
 import { useState } from "react"
 import { useAPIContext } from "../../lib/context/APIContext"
 import styles from "../../styles/Form.module.scss"
-import { ImFilePdf } from "react-icons/im";
-import { MdPictureAsPdf } from "react-icons/md";
 import { FaFilePdf } from "react-icons/fa";
 //INPUTS
 import Field from "./Field"
@@ -35,52 +33,54 @@ export default function Form() {
   if (!pages) return <div>Loading Form...</div>;
 
   return (
-      <form onSubmit={handleSubmit}>
-        <h2 className={styles.title}>{apiData.name}</h2>
+    <form onSubmit={handleSubmit}>
+      <h2 className={styles.title}>{apiData.name}</h2>
+      <div className={styles.header}>
         <p className={styles.description}>{apiData.description}</p>
-        <p className={styles.note}>
+        <div className={styles.note}>
           <span className={styles.required}>*</span>
           &nbsp;Required field
-        </p>
-        <Swiper
-          pagination={{
-            type: "progressbar",
-          }}
-          navigation={true}
-          modules={[Pagination, Navigation]}
-          className={styles.swiper}
-        >
-        {pages && pages.map((page, index) => {
-          return (
-            <SwiperSlide>
-              <Fieldset key={index} heading={page.heading}>
-                {page.fields && page.fields.map((field, index) => {
-                  return (
-                    <Field
-                      key={`${field}-${index}`}
-                      className={styles.field}
-                      type={field.type}
-                      label={field.label}
-                      placeholder={field.placeholder}
-                      name={field.name} 
-                      required={field.required}
-                      rows={field.rows}
-                      onKeyUp={handleChange}
-                    />
-                  )
-                })}
-              </Fieldset>
-            </SwiperSlide>
-          )
-        })}
-        </Swiper>
-        <button 
-          className={styles.submit}
-          type="Submit"
-        >
-          Submit to PDF&nbsp;
-          <FaFilePdf />
-        </button>
-      </form>
+        </div>
+      </div>
+      <Swiper
+        pagination={{
+          type: "progressbar",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className={styles.swiper}
+      >
+      {pages && pages.map((page, index) => {
+        return (
+          <SwiperSlide>
+            <Fieldset key={index} heading={page.heading}>
+              {page.fields && page.fields.map((field, index) => {
+                return (
+                  <Field
+                    key={`${field}-${index}`}
+                    className={styles.field}
+                    type={field.type}
+                    label={field.label}
+                    placeholder={field.placeholder}
+                    name={field.name} 
+                    required={field.required}
+                    rows={field.rows}
+                    onKeyUp={handleChange}
+                  />
+                )
+              })}
+            </Fieldset>
+          </SwiperSlide>
+        )
+      })}
+      </Swiper>
+      <button 
+        className={styles.submit}
+        type="Submit"
+      >
+        Submit to PDF&nbsp;
+        <FaFilePdf />
+      </button>
+    </form>
   )
 }
