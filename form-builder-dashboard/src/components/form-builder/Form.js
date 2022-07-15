6b928b78cbf614effcd4e9451314f0fd82be4a5c
Mallory Zlomanchuk <mzlomanchuk@hotmail.com>
2022-07-15 02:41:24 -0600

    added in swiper plugin

--- form-builder-dashboard/src/components/form-builder/Form.js
+++ form-builder-dashboard/src/components/form-builder/Form.js
@@ -1,9 +1,18 @@
 import { useState } from "react"
 import { useAPIContext } from "../../lib/context/APIContext"
 import styles from "../../styles/Form.module.scss"
+import { ImFilePdf } from "react-icons/im";
+import { MdPictureAsPdf } from "react-icons/md";
+import { FaFilePdf } from "react-icons/fa";
 //INPUTS
 import Field from "./Field"
 import Fieldset from "./Fieldset"
+//SWIPER
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper";
 
 export default function Form() {
   const [submitData, setSubmitData] = useState({});
@@ -33,28 +42,45 @@ export default function Form() {
           <span className={styles.required}>*</span>
           &nbsp;Required field
         </p>
+        <Swiper
+          pagination={{
+            type: "progressbar",
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className={styles.swiper}
+        >
         {pages && pages.map((page, index) => {
           return (
-            <Fieldset key={index} heading={page.heading}>
-              {page.fields && page.fields.map((field, index) => {
-                return (
-                  <Field
-                    key={`${field}-${index}`}
-                    className={styles.field}
-                    type={field.type}
-                    label={field.label}
-                    placeholder={field.placeholder}
-                    name={field.name} 
-                    required={field.required}
-                    rows={field.rows}
-                    onKeyUp={handleChange}
-                  />
-                )
-              })}
-            </Fieldset>
+            <SwiperSlide>
+              <Fieldset key={index} heading={page.heading}>
+                {page.fields && page.fields.map((field, index) => {
+                  return (
+                    <Field
+                      key={`${field}-${index}`}
+                      className={styles.field}
+                      type={field.type}
+                      label={field.label}
+                      placeholder={field.placeholder}
+                      name={field.name} 
+                      required={field.required}
+                      rows={field.rows}
+                      onKeyUp={handleChange}
+                    />
+                  )
+                })}
+              </Fieldset>
+            </SwiperSlide>
           )
         })}
-        <input className={styles.submit} type="Submit" />
+        </Swiper>
+        <button 
+          className={styles.submit}
+          type="Submit"
+        >
+          Submit to PDF&nbsp;
+          <FaFilePdf />
+        </button>
       </form>
   )
 }
